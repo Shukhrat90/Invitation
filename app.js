01011e5f815b5c2f0a85357c337670f3db207222
shukhrat90 <hushkadamov90@gmail.com>
2023-10-06 19:17:30 +0300

    first

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function updateCounter() {
 	// Секундк всего, далее остаток от преобразования в минуты, секунд осталось
 	const secondsLeft = Math.floor(diff / 1000) % 60;
 
-	console.log(daysLeft, hoursLeft, minutesLeft, secondsLeft);
+	// console.log(daysLeft, hoursLeft, minutesLeft, secondsLeft);
 
 	days.innerText = daysLeft;
 	hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
